78d19c994f1e3b94f846793e7801c3910f951ec5
retracd <29313219+retracd@users.noreply.github.com>
2024-05-16 13:49:35 -0500

    fixed dependency import issue - didnt change using statement after changing directory structure

--- TestWebApplication1/Controllers/RequestController.cs
+++ TestWebApplication1/Controllers/RequestController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
-using SharedDependencies;
+using SharedDependencies.Models;
 
 namespace TestWebApplication1.Controller
 {
