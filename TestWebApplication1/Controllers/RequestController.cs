a5806c452d9ddf7b3e98da9e565730b4c549279d
retracd <29313219+retracd@users.noreply.github.com>
2024-05-22 13:59:25 -0500

    added class ExternalController to RequestController.cs which contacts an external API - this simulates the service suite contacting the SaaS API

--- TestWebApplication1/Controllers/RequestController.cs
+++ TestWebApplication1/Controllers/RequestController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 using SharedDependencies.Models;
 
@@ -8,7 +9,7 @@ namespace TestWebApplication1.Controller
 {
     [ApiController]
     [Route("[controller]")]
-    public class RequestController : ControllerBase
+    public class RequestController : ControllerBase // contacts the 2nd web service's (TestWebApplication2) simple POST function localhost:6001/message
     {
         private readonly IHttpClientFactory _clientFactory;
 
@@ -24,7 +25,7 @@ namespace TestWebApplication1.Controller
 
             var messageData = new MessageData { 
                 Message = "Hello from TestWebApplication1", 
-                Number = 68,
+                Number = 42,
                 Time = DateTimeOffset.Now
             };
             var response = await client.PostAsJsonAsync("https://localhost:6001/message", messageData);
@@ -35,6 +36,33 @@ namespace TestWebApplication1.Controller
                 return Ok($"Response from TestWebApplication2 reads: {message}");
             }
             return StatusCode((int)response.StatusCode, "Failed to get a response from TestWebApplication2");
+        }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ExternalController : ControllerBase // contacts an external API (not one of the locally hosted)
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ExternalController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ContactAPI()
+        {
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://catfact.ninja/fact");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                return Ok($"Response from \"https://catfact.ninja/fact\" reads: {JsonDocument.Parse(message).RootElement.GetProperty("fact").GetString()}");
+            }
+            return StatusCode((int)response.StatusCode, "Failed to get a response from \"https://catfact.ninja/fact\"");
         }
     }
 }
